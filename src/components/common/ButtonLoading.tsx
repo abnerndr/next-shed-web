ae82c0d7767a890c90d44743a5d4894888aef1c2
Mateus Felipe <matheus@oialbert.com.br>
2024-01-30 21:59:57 -0300

    adjust desgni from login

--- src/components/common/ButtonLoading.tsx
+++ src/components/common/ButtonLoading.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export function ButtonLoading() {
   return (
-    <Button className="w-full" disabled>
+    <Button className="w-full bg-gray-400" disabled>
       <Loader2 className="mr-2 h-4 w-4 animate-spin" />
     </Button>
   );
