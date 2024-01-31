979415768168834c9a7d97df8a0bec40ebc2936b
Mateus Felipe <matheus@oialbert.com.br>
2024-01-30 23:57:32 -0300

    created registerform login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AcessForm from "@/components/acessform/AcessForm";
 import { ModeToggle } from "@/components/common/ModeToggle";
+import RegisterForm from "@/components/registerform/RegisterForm";
 
 export default function Home() {
   return (
     <>
       <ModeToggle />
-      <AcessForm />
+      {/* <AcessForm /> */}
+      <RegisterForm/>
     </>
   );
 }
--- src/components/common/ModeToggle.tsx
+++ src/components/common/ModeToggle.tsx
@@ -16,24 +16,26 @@ export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <div className="absolute flex">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            <span className="text-gray-800 font-bold">Light</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            <span className="text-gray-800 font-bold">Dark</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <div className="relative pl-96 ml-80 pt-2">
+      <div className="relative pl-96 ml-96">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              <span className="text-gray-800 font-bold">Light</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              <span className="text-gray-800 font-bold">Dark</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
